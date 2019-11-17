86b370d366ee39cac36f2674cfe4badf49e6e773
Shawn <sdmyers86@yahoo.com>
2019-11-16 19:24:15 -0500

    link users component with user model

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/User';
 
 @Component({
   selector: 'app-users',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  users: User[];
 
   constructor() { }
 
